fcfc32fdc1857e6fdfdefe622ac2053030c5afb9
Venkatesh <venkatesh.m@vinsinfo.com>
2024-11-01 20:55:14 +0530

    Recursive - Method  add Binary Search

--- Sorting&Searching/Code/search.py
+++ Sorting&Searching/Code/search.py
@@ -16,7 +16,19 @@ class Search:
             else:
                 s = mid+1
         return -1
+    def binarySearchRecursive(self,a,search,s,e):
+        if s<=e:
+            mid = (s+e)//2
+            if a[mid] == search:
+                return mid
+            elif a[mid]<search:
+                return self.binarySearchRecursive(a,search,mid+1,e)
+            else:
+                return self.binarySearchRecursive(a,search,s,mid-1)
+        return -1
+            
 obj = Search()
 a = [1,2,3,4,5,6,7,8,9,10]
 print('Linear Search: ',obj.linearSearch(a,4))
 print('Binary Search: ',obj.binarySearch(a,4))
+print('Binary Recursive Search:',obj.binarySearchRecursive(a,4,0,len(a)-1))
